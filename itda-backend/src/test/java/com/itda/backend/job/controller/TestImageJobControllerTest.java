7dd2e58b718a68b87b29f6cbbeed4771a843296d
이용호 <cici12358@naver.com>
2026-01-27 16:23:38 +0900

    Fix: DB 의존성 없이 동작하도록 WebMvcTest로 전환

--- itda-backend/src/test/java/com/itda/backend/job/controller/TestImageJobControllerTest.java
+++ itda-backend/src/test/java/com/itda/backend/job/controller/TestImageJobControllerTest.java
@@ -9,7 +9,7 @@ import com.itda.backend.node.repository.NodeMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(TestImageJobController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class TestImageJobControllerTest {
 
